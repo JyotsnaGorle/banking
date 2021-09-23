00bea16095d9d2ed6d8f4f0061328241e9442cce
jyotsna <jyotsna.gorle@appsfactory.de>
2021-09-23 15:15:08 +0200

    initial

---
+++ app/handlers.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
+
+type Customer struct {
+	Name    string `json:"full_name" xml:"full_name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zip_code"`
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Jo", City: "asdsad", Zipcode: "9082121"},
+		{Name: "Jo1", City: "asdsad", Zipcode: "9082121"},
+	}
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+
+}
