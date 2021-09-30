51425ecc2e4bb6a8b8fe24f9c24be90abda407b5
jyotsna <jyotsna.gorle@appsfactory.de>
2021-09-30 15:52:35 +0200

    introduce dto for getCutomer

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/JyotsnaGorle/banking/app/errs"
+import (
+	"github.com/JyotsnaGorle/banking/app/errs"
+	"github.com/JyotsnaGorle/banking/dto"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -9,6 +12,27 @@ type Customer struct {
 	Zipcode     string `json:"zip_code" db:"zipcode"`
 	DateOfBirth string `db:"date_of_birth"`
 	Status      string
+}
+
+// method to the domain object
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.statusAsText(),
+	}
 }
 
 // secondary port - protocol
--- service/customerService.go
+++ service/customerService.go
@@ -3,12 +3,13 @@ package service
 import (
 	"github.com/JyotsnaGorle/banking/app/errs"
 	"github.com/JyotsnaGorle/banking/domain"
+	"github.com/JyotsnaGorle/banking/dto"
 )
 
 // primary port
 type CustomerService interface {
 	GetAllCustomer(string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 // implementation of the interface CutomerService
@@ -30,8 +31,15 @@ func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer
 	return s.repo.FindAll(status)
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.ById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	response := c.ToDto()
+
+	return &response, nil
 }
 
 // helper func to instantiate the default customer service
