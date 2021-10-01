3dde71fc29d3a4d5cae19d4939d73fae84ec37ae
jyotsna <jyotsna.gorle@appsfactory.de>
2021-10-01 15:19:47 +0200

    decode the incoming request and validate in service layer

---
+++ app/accountHandler.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/JyotsnaGorle/banking/dto"
+	"github.com/JyotsnaGorle/banking/service"
+)
+
+type AccountHandler struct {
+	service service.AccountService
+}
+
+func (h AccountHandler) NewAccount(w http.ResponseWriter, r *http.Request) {
+	var request dto.NewAccountRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+
+	if err != nil {
+		writeResponse(w, http.StatusBadRequest, err.Error())
+	} else {
+		account, appError := h.service.NewAccount(request)
+		if appError != nil {
+			writeResponse(w, appError.Code, appError.Message)
+		} else {
+			writeResponse(w, http.StatusCreated, account)
+		}
+	}
+
+}
--- dto/newAccountRequest.go
+++ dto/newAccountRequest.go
@@ -1,7 +1,24 @@
 package dto
 
+import (
+	"strings"
+
+	"github.com/JyotsnaGorle/banking/errs"
+)
+
 type NewAccountRequest struct {
 	CustomerId  string  `json:"customer_id"`
 	AccountType string  `json:"account_type"`
 	Amount      float64 `json:"amount"`
+}
+
+func (r NewAccountRequest) Validate() *errs.AppError {
+	if r.Amount < 5000 {
+		return errs.NewValidationError("To open a new account you need to deposit more than 5000.")
+	}
+
+	if strings.ToLower(r.AccountType) != "saving" && r.AccountType != "checking" {
+		return errs.NewValidationError("Account type should be checking or savings")
+	}
+	return nil
 }
