768fff82eaabcbff6b07cb839b8153c4c6048c9b
jyotsna <jyotsna.gorle@appsfactory.de>
2021-10-01 15:29:45 +0200

    retrieve id from url and fix spelling mismatch

--- app/accountHandler.go
+++ app/accountHandler.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/JyotsnaGorle/banking/dto"
 	"github.com/JyotsnaGorle/banking/service"
+	"github.com/gorilla/mux"
 )
 
 type AccountHandler struct {
@@ -13,12 +14,18 @@ type AccountHandler struct {
 }
 
 func (h AccountHandler) NewAccount(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	id := vars["customer_id"]
+
 	var request dto.NewAccountRequest
 	err := json.NewDecoder(r.Body).Decode(&request)
 
 	if err != nil {
 		writeResponse(w, http.StatusBadRequest, err.Error())
 	} else {
+		request.CustomerId = id
 		account, appError := h.service.NewAccount(request)
 		if appError != nil {
 			writeResponse(w, appError.Code, appError.Message)
--- domain/AccountRepositoryDb.go
+++ domain/AccountRepositoryDb.go
@@ -13,9 +13,9 @@ type AccountRepositoryDb struct {
 }
 
 func (d AccountRepositoryDb) Save(a Account) (*Account, *errs.AppError) {
-	sqlInsert := "INSERT INTO accounts (customer_id, opening_date, account_type, amount, status) values (?, ? ,?, ?)"
+	sqlInsert := "INSERT INTO accounts (customer_id, opening_date, account_type, amount, status) values (?, ? ,?, ?, ?)"
 
-	result, err := d.client.Exec(sqlInsert, a.CustomerId, a.CustomerId, a.OpeningDate, a.AccountType, a.Amount, a.Status)
+	result, err := d.client.Exec(sqlInsert, a.CustomerId, a.OpeningDate, a.AccountType, a.Amount, a.Status)
 	if err != nil {
 		logger.Error("Error while creating new account" + err.Error())
 		return nil, errs.NewUnexpectedError("Unexpected error from Database")
